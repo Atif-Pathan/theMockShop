c05658c654fc4ec42dc40b04c98d84290779dc07
Atif Pathan <atifkhan.p1@gmail.com>
2025-06-21 12:45:05 -0600

    create dummy pages and setup react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,50 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import CartPage from './pages/CartPage.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
+import HomePage from './pages/HomePage.jsx';
+import ProductDetailPage from './pages/ProductDetailPage.jsx';
+import ShopPage from './pages/ShopPage.jsx';
+import {
+  getAllProducts,
+  getProductBySlug,
+  getRelatedProductsBySlug,
+} from './services/productsAPI.js';
 import './index.css';
 import App from './App.jsx';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'shop', element: <ShopPage />, loader: getAllProducts },
+      {
+        path: 'shop/:productSlug',
+        element: <ProductDetailPage />,
+        loader: async ({ params }) => {
+          const product = getProductBySlug(params.productSlug);
+          const relatedProducts = getRelatedProductsBySlug(params.productSlug);
+
+          // Wait for both promises to resolve
+          const [productData, relatedProductsData] = await Promise.all([
+            product,
+            relatedProducts,
+          ]);
+
+          return { product: productData, related: relatedProductsData };
+        },
+      },
+      { path: 'cart', element: <CartPage /> },
+    ],
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
