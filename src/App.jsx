0c4825c8e872ccbff2e1897e3c10f0ad5816072c
Atif Pathan <atifkhan.p1@gmail.com>
2025-06-22 16:50:11 -0600

    Setup App.jsx with a Header and Outlet to show pages via react router and pass in data without prop drilling to any page that might need it. It is also the main source of truth for items in the cart so it can handle add update and delete of items in the cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,73 @@
 import './App.css';
+import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header/Header.jsx';
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+  const [theme, setTheme] = useState('dark');
+
+  const handleAddToCart = (product, quantity) => {
+    // If quantity is 0 or less, treat it as a removal.
+    if (quantity <= 0) {
+      handleRemoveItem(product.id);
+      return;
+    }
+
+    const existingItem = cartItems.find((item) => item.id === product.id);
+
+    if (existingItem) {
+      const updatedCartItems = cartItems.map((item) =>
+        item.id === product.id
+          ? // Set quantity, don't add to it.
+            { ...item, quantity: quantity }
+          : item,
+      );
+      setCartItems(updatedCartItems);
+    } else {
+      const updatedCartItems = [...cartItems, { ...product, quantity }];
+      setCartItems(updatedCartItems);
+    }
+  };
+
+  const handleUpdateQuantity = (productId, newQuantity) => {
+    if (newQuantity <= 0) {
+      // if quanitity is 0 then we should delete the item instead
+      handleRemoveItem(productId);
+      return;
+    }
+    const updatedCartItems = cartItems.map((item) =>
+      item.id === productId ? { ...item, quantity: newQuantity } : item,
+    );
+    setCartItems(updatedCartItems);
+  };
+
+  const handleRemoveItem = (productId) => {
+    const updatedCartItems = cartItems.filter((item) => item.id !== productId);
+    setCartItems(updatedCartItems);
+  };
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
+  };
+
+  document.documentElement.className = theme;
+
   return (
     <>
-      <div>hello world</div>
-      <h1>hello world twice</h1>
+      <Header
+        numberOfItems={cartItems.length}
+        theme={theme}
+        toggleTheme={toggleTheme}
+      />
+      <Outlet
+        context={{
+          cartItems,
+          handleAddToCart,
+          handleUpdateQuantity,
+          handleRemoveItem,
+        }}
+      />
     </>
   );
 }
